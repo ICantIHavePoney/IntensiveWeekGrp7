87481c015323b8edf34f70afa41eee86a5d442a5
Kazura23 <tom.kcz@hotmail.fr>
2017-01-24 21:29:50 +0100

    Update Level Design
    Try Sprite on Character / Failed Animations, moving platforms, skybox etc.

---
+++ Assets/DisappearingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+	public float duration;
+	public float goToZ;
+	public float backToZ;
+
+	Vector3 originTransform;
+	bool started = true;
+
+	void Start ()
+	{
+		StartCoroutine ("Move");
+		originTransform = transform.position;
+	}
+
+	void Update ()
+	{
+		if (started == true) {
+			StartCoroutine ("Move");
+		}
+
+	}
+
+	IEnumerator Move ()
+	{
+		started = false;
+		transform.DOLocalMoveZ (goToZ, duration);
+		yield return new WaitForSeconds (5f);
+		transform.DOLocalMoveZ (backToZ, duration);
+		yield return new WaitForSeconds (5f);
+		started = true;
+		transform.DOKill ();
+	}
+
+
+
+}
