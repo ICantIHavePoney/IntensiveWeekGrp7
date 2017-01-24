9d52dad735d02b40ba1f975be0b973ffced992be
Kazura23 <tom.kcz@hotmail.fr>
2017-01-24 18:57:43 +0100

    Update LD
    Skybox implémentée, début LD sans textures

---
+++ Assets/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+	public Vector3 checkpointPosition;
+	// Use this for initialization
+	void Start ()
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.transform.tag == "DeathZone") {
+			transform.position = checkpointPosition;
+		}
+
+		if (other.transform.tag == "Checkpoint") {
+			checkpointPosition = other.transform.position;
+		}
+
+
+	}
+
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,133 +2,121 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 
-    public bool isJumping;
-    Vector3 startPosition;
-    Rigidbody m_rigidbody;
+	public bool isJumping;
+	Vector3 startPosition;
+	Rigidbody m_rigidbody;
 
-    public GameObject canvas;
+	public GameObject canvas;
 
-    bool doubleJump;
+	bool doubleJump;
 
-    public int speed;
-    public int jumpHeight;
+	public int speed;
+	public int jumpHeight;
 
-    float gravity = 20;
+	float gravity = 20;
 
-    float newDash;
+	float newDash;
 
-    public float dashTimer = 0.5f;
+	public float dashTimer = 0.5f;
 
-    bool isDashing;
+	bool isDashing;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
         
-        m_rigidbody = GetComponent<Rigidbody>();
-        startPosition = transform.position;
+		m_rigidbody = GetComponent<Rigidbody> ();
+		startPosition = transform.position;
 
-        speed = 10;
-        jumpHeight = 10;
-
-    }
+	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+	{
 
 
 
-        m_rigidbody.AddForce(Vector3.down * (gravity));
+		m_rigidbody.AddForce (Vector3.down * (gravity));
 
 
 
-        if (!canvas.activeInHierarchy)
-        {
-            if(Input.GetAxisRaw("Horizontal") == 1)
-            {
-                moveRight();
-            }
-            if(Input.GetAxisRaw("Horizontal") == -1)
-            {
-                moveLeft();
-            }
-        }
+		if (!canvas.activeInHierarchy) {
+			if (Input.GetAxisRaw ("Horizontal") == 1) {
+				moveRight ();
+			}
+			if (Input.GetAxisRaw ("Horizontal") == -1) {
+				moveLeft ();
+			}
+		}
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            StartCoroutine("Dash");
-        }
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+			StartCoroutine ("Dash");
+		}
 
-        if (Input.GetKeyDown("space"))
-        {
-            Jump();
-        }
+		if (Input.GetKeyDown ("space")) {
+			Jump ();
+		}
 
 	}
 
-    void moveLeft()
-    {
+	void moveLeft ()
+	{
 
-        transform.Translate(new Vector3( -1 * Time.deltaTime * speed, 0, 0));
-    }
+		transform.Translate (new Vector3 (-1 * Time.deltaTime * speed, 0, 0));
+	}
 
-    void moveRight()
-    {
-        transform.Translate(new Vector3( 1 * Time.deltaTime * speed, 0, 0));
-    }
-
-
-    void Jump()
-    {
+	void moveRight ()
+	{
+		transform.Translate (new Vector3 (1 * Time.deltaTime * speed, 0, 0));
+	}
 
 
-        if (!isJumping)
-        {
-
-            m_rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            isJumping = true;
-        }
-        else if(isJumping && !doubleJump)
-        {
-            m_rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
-            doubleJump = true;
-        }
-
-    }
+	void Jump ()
+	{
 
 
-    IEnumerator Dash()
-    {
+		if (!isJumping) {
+
+			m_rigidbody.AddForce (Vector3.up * jumpHeight, ForceMode.Impulse);
+			isJumping = true;
+		} else if (isJumping && !doubleJump) {
+			m_rigidbody.AddForce (Vector3.up * jumpHeight, ForceMode.Impulse);
+			doubleJump = true;
+		}
+
+	}
+
+
+	IEnumerator Dash ()
+	{
 
 
 
-        isDashing = true;
-        gravity = 0;
-        m_rigidbody.velocity = Vector3.zero;
-        if (Input.GetAxisRaw("Horizontal") == -1)
-        {
-            m_rigidbody.AddForce(Vector3.left * 15, ForceMode.Impulse);
-        }
-        else
-        {
-            m_rigidbody.AddForce(Vector3.right * 15, ForceMode.Impulse);
-        }
-        yield return new WaitForSeconds(0.5f);
-        m_rigidbody.velocity = Vector3.zero;
-        isDashing = false;
-        gravity = 20;
+		isDashing = true;
+		gravity = 0;
+		m_rigidbody.velocity = Vector3.zero;
+		if (Input.GetAxisRaw ("Horizontal") == -1) {
+			m_rigidbody.AddForce (Vector3.left * 15, ForceMode.Impulse);
+		} else {
+			m_rigidbody.AddForce (Vector3.right * 15, ForceMode.Impulse);
+		}
+		yield return new WaitForSeconds (0.5f);
+		m_rigidbody.velocity = Vector3.zero;
+		isDashing = false;
+		gravity = 20;
 
 
-    }
+	}
 
 
-    void OnCollisionEnter(Collision other)
-    {
-        if(other.transform.tag == "floor")
-        {
-            isJumping = false;
-            doubleJump = false;
-        }
-    }
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.transform.tag == "floor") {
+			isJumping = false;
+			doubleJump = false;
+		}
+	}
 }
